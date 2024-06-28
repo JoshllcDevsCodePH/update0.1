00068ae240f11600350dc4cc57efe9088a31ff05
JoshllcDevsCode <121450267+JoshllcDevsCodePH@users.noreply.github.com>
2024-06-29 03:51:09 +0800

    Update index.tsx

--- src/components/Pages/index.tsx
+++ src/components/Pages/index.tsx
@@ -1,7 +1,7 @@
 import { Box } from "@mui/material";
 import { Clicker } from "../Clicker";
 import { Footer } from "../Footer";
-import { FC, useEffect } from "react";
+import { FC } from "react";
 
 export type IndexPageProps = {
   count: number;
@@ -22,12 +22,8 @@ export const IndexPage: FC<IndexPageProps> = ({
   click,
   maxPower,
   progress,
-  saveDataToTelegram,
+  saveDataToTelegram // Dito walang default value
 }) => {
-  useEffect(() => {
-    saveDataToTelegram();
-  }, [count, currentPower]);
-
   return (
     <Box
       sx={{
@@ -54,7 +50,7 @@ export const IndexPage: FC<IndexPageProps> = ({
           backgroundRepeat: "no-repeat",
           transform: "scale(1.1)",
           filter: "blur(8px)",
-          "-webkit-filter": "blur(8px)",
+          "-webkit-filter": " blur(8px)",
           "&:before": {
             content: '""',
             position: "absolute",
